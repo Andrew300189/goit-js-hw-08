cb98a55b9e1acef9bacf22fae1a1998e21ea4dc7
Andrey <94688521+Andrew300189@users.noreply.github.com>
2023-08-04 19:24:40 +0300

    Changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,13 @@
+// Импортируем библиотеку SimpleLightbox из модуля 'simplelightbox'
 import SimpleLightbox from 'simplelightbox';
+
+// Импортируем стили CSS для библиотеки SimpleLightbox из файла 'simple-lightbox.min.css'
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Импортируем массив с элементами галереи из файла 'gallery-items.js'
 import { galleryItems } from './gallery-items.js';
 
+// Функция createGalleryItem принимает объект item и возвращает строку с разметкой элемента галереи
 function createGalleryItem(item) {
   return `
     <li class="gallery__item">
@@ -16,13 +22,19 @@ function createGalleryItem(item) {
   `;
 }
 
+
 const gallery = document.querySelector('.gallery');
 
+// Создаем HTML разметку для всех элементов галереи, используя функцию createGalleryItem и метод map для перебора массива galleryItems
+// Затем объединяем элементы массива в одну строку с помощью метода join('')
 const galleryItemsHTML = galleryItems.map((item) => createGalleryItem(item)).join('');
+
+// Вставляем разметку всех элементов галереи в конец контейнера галереи
 gallery.insertAdjacentHTML('beforeend', galleryItemsHTML);
 
+// Создаем новый экземпляр SimpleLightbox и передаем ему селектор для элементов галереи, которые будут являться ссылками для открытия изображений
 const lightbox = new SimpleLightbox('.gallery__item a', {
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 250,
+  captionsData: 'alt',         
+  captionPosition: 'bottom',   
+  captionDelay: 250,          
 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,48 +1,58 @@
+// Импортируем функцию throttle из библиотеки lodash
 import throttle from 'lodash.throttle';
 
+// Находим форму и поля ввода по соответствующим селекторам
 const form = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('input[name="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
 
-// Функция для сохранения данных формы в локальное хранилище
+// Функция для сохранения данных полей в локальное хранилище с помощью throttle
 const saveFormData = throttle(() => {
+  
   const formData = {
     email: emailInput.value,
     message: messageInput.value,
   };
+  // Сохраняем объект в локальное хранилище, преобразовав его в строку JSON
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-}, 500);
+}, 500); // Ограничиваем частоту обновления хранилища 500 миллисекундами
 
-// Функция для получения данных формы из локального хранилища
+// Функция для получения сохраненных данных из локального хранилища
 const getSavedFormData = () => {
+  // Получаем строку JSON из хранилища
   const savedData = localStorage.getItem('feedback-form-state');
+  // Если данные есть, парсим их из JSON в объект, иначе возвращаем пустой объект
   return savedData ? JSON.parse(savedData) : { email: '', message: '' };
 };
 
-// Функция для заполнения полей формы данными из локального хранилища при загрузке страницы
+// Функция для заполнения полей формы данными из хранилища
 const populateFormFields = () => {
+  // Получаем сохраненные данные из хранилища
   const formData = getSavedFormData();
+  // Заполняем поля формы значениями из объекта formData
   emailInput.value = formData.email;
   messageInput.value = formData.message;
 };
 
-// Слушаем событие input на форме и сохраняем данные в локальное хранилище
+// Слушаем событие input на форме, чтобы сохранять данные в хранилище
 form.addEventListener('input', () => {
   saveFormData();
 });
 
-// Слушаем событие submit на форме
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
+// Слушаем событие submit на форме, чтобы выводить данные в консоль и очищать хранилище и поля формы
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // Получаем сохраненные данные из хранилища
   const formData = getSavedFormData();
+  // Выводим данные в консоль
   console.log(formData);
 
-  // Очищаем локальное хранилище и поля формы
+  // Удаляем данные из хранилища
   localStorage.removeItem('feedback-form-state');
+  // Очищаем поля формы
   emailInput.value = '';
   messageInput.value = '';
 });
 
-// Заполняем поля формы данными из локального хранилища при загрузке страницы
+// При загрузке страницы заполняем поля формы данными из хранилища
 populateFormFields();
-
